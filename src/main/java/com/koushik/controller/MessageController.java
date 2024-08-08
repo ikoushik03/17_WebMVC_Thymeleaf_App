433645f7ed209bddfa01cc265893c95541d2b7fb
hp <koushiksharma9939@gmail.com>
2024-08-08 22:03:21 +0530

    first commit

---
+++ src/main/java/com/koushik/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.koushik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MessageController {
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg(Model model)
+	{
+		model.addAttribute("msg", "Hello world");
+		return "home";
+	}
+}
---
+++ src/main/java/com/koushik/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.koushik.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.koushik.binding.Product;
+
+@Controller
+public class ProductController {
+	
+	
+    //method to display form
+	
+	@GetMapping("/")
+	public String loadForm(Model model)
+	{
+		model.addAttribute("product", new Product());
+		return"index";
+	}
+	
+	//method to handle form submission
+	@PostMapping("/product")
+	public String handleSubmitBtn(Product p,Model model)
+	{
+		System.out.println(p);
+		
+		model.addAttribute("msg", "Product Saved");
+		
+		return "index";
+	}
+}
